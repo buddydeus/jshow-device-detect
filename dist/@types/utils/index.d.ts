5531682e6a8d82566cf6370b25a4dd93e3961f36
Buddy Deus <buddy@MacBook-Pro.local>
2020-11-11 23:03:28 +0800

    rm dist & change package.json

--- dist/@types/utils/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const hasStr: (str1: any, str2: string) => boolean;
-export declare const getLower: (str: string) => string;
-export declare const getMajor: (version: any) => any;
-export declare const trimStr: (str: string) => string;
-export declare const getStr: (str: string, maps: any) => string | undefined;
-export declare const execRxp: (target: any, ua: string, arrays: any[]) => void;
